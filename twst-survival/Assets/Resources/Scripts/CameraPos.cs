db3cfb5eb12da2bfed22666f11150e8ee5583cb0
yellol <46012526+yellol@users.noreply.github.com>
2023-05-05 14:19:07 -0500

    fix camera relative to target position

--- twst-survival/Assets/Resources/Scripts/CameraPos.cs
+++ twst-survival/Assets/Resources/Scripts/CameraPos.cs
@@ -5,8 +5,15 @@ using UnityEngine;
 public class CameraPos : MonoBehaviour
 {
     public Transform target;
-    
+
+    private Camera _camera;
     // Update is called once per frame
+
+    void Awake()
+    {
+        _camera = Camera.main;
+    }
+
     void Update()
     {
         if (target == null)
@@ -14,8 +21,9 @@ public class CameraPos : MonoBehaviour
             return;
         }
 
-        float _x = transform.position.x;
-        float _y = transform.position.y;
-        transform.position = new Vector3((_x + Input.mousePosition.x) / 2, (_y + Input.mousePosition.y) / 2, 0);
+        float _x = target.transform.position.x;
+        float _y = target.transform.position.y;
+        Vector3 _mouse = _camera.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = new Vector3((_x + _mouse.x) / 2, (_y + _mouse.y) / 2, -1);
     }
 }
